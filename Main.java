584d8c653dce9fcfee6a6b5dfefa411b02465ca4
Oliver Ager Wiklander <agerwiklander.oliver@elev.ntig.se>
2023-01-25 13:06:28 +0100

    final

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    public static void main(String args[]){
-        Book book;
+    public static void main(String[] args){
 
-        book.toString(book.bname="Salvador");
+        Author margarete = new Author("Margarete","Margatron@hotmail.com",'F');
+        Author per = new Author("Per Lundare", "perpatrol@gmail.com", 'M');
+        Book book1 = new Book("Happiness",margarete,19.99, 7);
+        System.out.println(book1);
+        Book book2 = new Book( "Salvadores", margarete, 14.89, 13);
+        System.out.println(book2);
+        Book book3 = new Book("Building Good", per, 39.65, 230);
+        System.out.println(book3);
+        Book book4 = new Book("Shouldn´t call Lars", per, 99.99);
+        System.out.println(book4);
     }
 }
